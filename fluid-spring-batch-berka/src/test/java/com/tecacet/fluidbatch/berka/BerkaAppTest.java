f05d0620433d0ee930fb7275e6b406d2faa3cbcf
algorythmist <dimitri@tecacet.com>
2021-04-10 16:08:21 +0000

    reorganization

--- fluid-spring-batch-berka/src/test/java/com/tecacet/fluidbatch/berka/BerkaAppTest.java
+++ fluid-spring-batch-berka/src/test/java/com/tecacet/fluidbatch/berka/BerkaAppTest.java
@@ -1,5 +1,6 @@
 package com.tecacet.fluidbatch.berka;
 
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,4 +13,5 @@ class BerkaAppTest {
     @Test
     void contextLoads() {
     }
+
 }
---
+++ fluid-spring-batch-examples/src/main/java/com/tecacet/fluidbatch/berka/ExamplesApp.java
@@ -0,0 +1,13 @@
+package com.tecacet.fluidbatch.berka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ExamplesApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamplesApp.class, args);
+    }
+}
